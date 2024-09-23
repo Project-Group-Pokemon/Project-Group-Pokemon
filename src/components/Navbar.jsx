1821b03f81adbb74b6dd072dfa21efb61f8e24d6
wahibkurniawan <wahibkurniawan49@gmail.com>
2024-09-24 00:56:12 +0700

    menambah sidebar dan perbaikan layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-    if (!darkMode) {
+  const [darkMode, setDarkMode] = useState(true);
+  useEffect(() => {
+    if (darkMode) {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
     }
+  }, [darkMode]);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
   };
 
   return (
-    <nav className="bg-gray-200 text-gray-800 p-4 shadow-sm flex items-center justify-between dark:bg-gray-800 dark:text-gray-300">
-      <h1 className="text-2xl font-bold">Pokémon Awesome!</h1>
-      <button 
-        onClick={toggleDarkMode} 
-        className="bg-gray-800 p-2 rounded-full focus:outline-none"
-      >
-        {darkMode ? <FaSun className="text-yellow-400" /> : <FaMoon />}
-      </button>
+    <nav className="bg-gray-100 text-gray-800 p-4 shadow-sm flex border-b-2 border-gray-300 dark:border-gray-800 justify-center dark:bg-gray-800 dark:text-gray-300">
+      <div className="flex items-center justify-between w-full max-w-7xl">
+        <h1 className="text-2xl font-bold">Pokémon Awesome!</h1>
+        <button
+          onClick={toggleDarkMode}
+          className="bg-gray-400 p-2 rounded-full focus:outline-none dark:bg-gray-300"
+        >
+          {darkMode ? <FaSun className="text-yellow-400" /> : <FaMoon />}
+        </button>
+      </div>
     </nav>
   );
 };
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { FaClipboardList, FaBookmark, FaArrowsAlt, FaEgg, FaShapes, FaInfoCircle } from 'react-icons/fa';
+
+const Sidebar = () => {
+    const [activeItem, setActiveItem] = useState('Pokémons');
+
+    const menuItems = [
+        { name: 'Pokémons', icon: <FaClipboardList /> },
+        { name: 'Bookmarks', icon: <FaBookmark /> },
+    ];
+
+    const pokemonDataItems = [
+        { name: 'Evolutions', icon: <FaArrowsAlt /> },
+        { name: 'Types', icon: <FaShapes /> },
+        { name: 'Egg Groups', icon: <FaEgg /> },
+    ];
+
+    const miscItems = [
+        { name: 'About', icon: <FaInfoCircle /> },
+    ];
+
+    const handleClick = (item) => {
+        setActiveItem(item);
+    };
+
+    return (
+        <div className="w-64 bg-gray-100 text-gray-800 h-screen p-6 dark:bg-gray-900 dark:text-gray-300 ">
+            <nav>
+                <div>
+                    <h3 className="text-xs font-semibold uppercase text-gray-800 dark:text-gray-400 mb-2">Main Menu</h3>
+                    <ul className="space-y-2 mb-6 border-l-2 border-gray-300 dark:border-gray-800">
+                        {menuItems.map((item) => (
+                            <li
+                                key={item.name}
+                                onClick={() => handleClick(item.name)}
+                                className={`flex items-center space-x-2 p-2 cursor-pointer hover:text-gray-900 dark:hover:text-white ${activeItem === item.name ? 'border-l-4 border-blue-500 bg-gray-300 dark:bg-gray-800' : 'border-l-4 border-transparent'
+                                    }`}
+                            >
+                                {item.icon}
+                                <span className="font-medium">{item.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-xs font-semibold uppercase text-gray-800 dark:text-gray-400 mb-2">Pokémon Data</h3>
+                    <ul className="space-y-2 mb-6 border-l-2 border-gray-300 dark:border-gray-800">
+                        {pokemonDataItems.map((item) => (
+                            <li
+                                key={item.name}
+                                onClick={() => handleClick(item.name)}
+                                className={`flex items-center space-x-2 p-2 cursor-pointer hover:text-gray-900 dark:hover:text-white ${activeItem === item.name ? 'border-l-4 border-blue-500 bg-gray-300 dark:bg-gray-800' : 'border-l-4 border-transparent'
+                                    }`}
+                            >
+                                {item.icon}
+                                <span className="font-medium">{item.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 className="text-xs font-semibold uppercase text-gray-800 dark:text-gray-400 mb-2">Misc.</h3>
+                    <ul className="space-y-2 border-l-2 border-gray-300 dark:border-gray-800">
+                        {miscItems.map((item) => (
+                            <li
+                                key={item.name}
+                                onClick={() => handleClick(item.name)}
+                                className={`flex items-center space-x-2 p-2 cursor-pointer hover:text-gray-900 dark:hover:text-white ${activeItem === item.name ? 'border-l-4 border-blue-500 bg-gray-300 dark:bg-gray-800' : 'border-l-4 border-transparent'
+                                    }`}
+                            >
+                                {item.icon}
+                                <span className="font-medium">{item.name}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
