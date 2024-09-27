89305bd7e6909e86d6f10a02b122e89425fe45e1
wahibkurniawan <wahibkurniawan49@gmail.com>
2024-09-27 16:27:53 +0700

    limit 50

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -22,7 +22,7 @@ const PokemonList = ({ searchParams }) => {
     useEffect(() => {
         const fetchAllPokemon = async () => {
             try {
-                const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0`);
+                const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`);
                 if (!response.ok) {
                     throw new Error('Error fetching all Pokémon list');
                 }
